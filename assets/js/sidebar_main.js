7cfb16d041a8ae25daade6c2394b60e0219d1197
Rey Crittenden <rcrit01@jaguar.tamu.edu>
2020-06-17 19:38:03 -0500

    Update

--- assets/js/sidebar_main.js
+++ assets/js/sidebar_main.js
@@ -16,4 +16,24 @@
       $('#sidebar').toggleClass('active');
   });
 
+  /*Something to do with a card*/
+
+  $(document).ready(function() {
+	$(".menu-icon").on("click", function() {
+		  $("nav ul").toggleClass("showing");
+	});
+});
+
+// Scrolling Effect
+
+$(window).on("scroll", function() {
+	if($(window).scrollTop()) {
+		  $('nav').addClass('black');
+	}
+
+	else {
+		  $('nav').removeClass('black');
+	}
+})
+
 })(jQuery);
